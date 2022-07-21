e86202129231861b6e6f723e55a1ffdd255cf9ad
William Gasparetto <gasparetto.w@gmail.com>
2022-07-20 22:01:40 -0300

    feat: add initial config

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default () => (
+  <>
+    <Head>
+      <title>Super Trunfo Dota 2</title>
+      <link rel="icon" href="/favicon.png" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <meta charSet="utf-8" />
+      <meta
+        name="description"
+        content="A Next App to play a Top Trump Dota 2 non-official game against your friends"
+      />
+    </Head>
+    <h1>Super Trunfo Dota 2</h1>
+  </>
+);
